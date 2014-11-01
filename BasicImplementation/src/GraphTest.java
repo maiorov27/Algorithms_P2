01daf9813ac5883d1eb4e347c875a94323df3467
egor <maiorov27@gmail.com>
2014-11-01 23:43:50 +0200

    add graph test file

---
+++ BasicImplementation/src/GraphTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+
+public class GraphTest {
+
+    private Graph  graph;
+
+    @Before
+    public void setUp() {
+        graph = new Graph(10);
+        In in = new In();
+    }
+
+
+
+}
